38460d548b8736a88290dab0065cd4efa93cb4ee
Poosana  Thassanavisut <poosanathassanavisut@Poosanas-MacBook-Pro.local>
2020-12-15 15:02:55 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int n;
+    int even=0,odd=0;
+   
+    while(n != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin>>n;
+    if(n%2 == 0 && n!= 0){
+        even++;
+    }
+    if(n%2 != 0 && n != 0){
+        odd++;
+        }
+    }
+    cout << "#Even numbers = "<<even<<endl;
+
+    cout << "#Odd numbers = "<<odd<<endl;
     return 0;
 }
